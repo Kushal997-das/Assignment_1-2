314dd123a102c36da4fd790d8c4d0723b63378df
Kushal997-das <61356005+Kushal997-das@users.noreply.github.com>
2020-03-23 12:39:15 +0530

    WAP that read a line of a string it should then display the number of occurance of given sub-string.

---
+++ program01.py
@@ -0,0 +1,5 @@
+string=input("enter your string")
+sub_string=input("enter the substring")
+temp=string
+count=temp.count(sub_string)
+print("total occurance(%s) of this substring is:%d"%(sub_string,count))
