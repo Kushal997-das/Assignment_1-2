c46424448794b531b993cf4c49b4df2d09d916d6
Kushal997-das <61356005+Kushal997-das@users.noreply.github.com>
2020-03-23 12:42:30 +0530

    WAP captalizes the 1st letter of each word and form a new string of it.

---
+++ program03.py
@@ -0,0 +1,13 @@
+x="github"
+a=x.capitalize()
+y="the"
+b=y.capitalize()
+z="place"
+c=z.capitalize()
+m="where"
+d=m.capitalize()
+n="i"
+e=n.capitalize()
+o="fork"
+f=o.capitalize()
+print(f"{a} {b} {c} {d} {e} {f}")
